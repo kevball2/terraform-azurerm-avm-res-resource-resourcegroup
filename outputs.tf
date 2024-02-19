8fe1f9bcf0a4b040e1e989cfd7dd81aa96e995d9
Kevin <kevball2@gmail.com>
2024-02-19 08:14:23 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,4 @@
 # Module owners should include the full resource via a 'resource' output
-# https://confluence.ei.leidos.com/display/ECM/Terraform+ECM+Style+Guide#TerraformECMStyleGuide-TFFR2-Category:Outputs-AdditionalTerraformOutputs
 output "resource" {
   description = "This is the full output for the resource group."
   value       = azurerm_resource_group.this
