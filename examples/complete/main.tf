051db825115fff4f948141adc8ac53f00f7f3c40
Kevin Oliver <oliverkr@leidos.com>
2024-02-19 11:43:47 -0500

    testing remote file access

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -9,7 +9,7 @@ module "naming" {
 #   name     = "rg-complete-test"
 # }
 
-resource "azurerm_user_assigned_identity" "dep-uai" {
+resource "azurerm_user_assigned_identity" "dep_uai" {
   name                = module.naming.user_assigned_identity.name_unique
   resource_group_name = module.resource_group.resource.name
   location            = module.resource_group.resource.location
@@ -31,37 +31,25 @@ module "resource_group" {
   }
   role_assignments = {
     "roleassignment1" = {
-      principal_id               = azurerm_user_assigned_identity.dep-uai.principal_id
+      principal_id               = azurerm_user_assigned_identity.dep_uai.principal_id
       role_definition_id_or_name = "Reader"
     },
     "role_assignment2" = {
-      role_definition_id_or_name       = "2a2b9908-6ea1-4ae2-8e65-a410df84e7d1" // Storage Blob Data Reader Role Guid 
-      principal_id                     = "4179302c-702e-4de7-a061-beacd0a1be09"
+      role_definition_id_or_name       = "/providers/Microsoft.Authorization/roleDefinitions/2a2b9908-6ea1-4ae2-8e65-a410df84e7d1" # Storage Blob Data Reader Role Guid 
+      principal_id                     = azurerm_user_assigned_identity.dep_uai.principal_id
       skip_service_principal_aad_check = false
       condition_version                = "2.0"
       condition                        = <<-EOT
-    (
-      (
-        !(ActionMatches{'Microsoft.Authorization/roleAssignments/write'})
-      )
-    OR 
-      (
-      @Request[Microsoft.Authorization/roleAssignments:RoleDefinitionId]
-      ForAnyOfAnyValues:GuidEquals {4179302c-702e-4de7-a061-beacd0a1be09}
-      )
-    )
-    AND
-    (
-      (
-        !(ActionMatches{'Microsoft.Authorization/roleAssignments/delete'})
-      )
-      OR 
-      (
-        @Resource[Microsoft.Authorization/roleAssignments:RoleDefinitionId]
-        ForAnyOfAnyValues:GuidEquals {dc887ae1-fe50-4307-be53-213ff08f3c0b}
-      )
-    )
-    EOT  
+(
+ (
+  !(ActionMatches{'Microsoft.Storage/storageAccounts/blobServices/containers/blobs/read'} AND NOT SubOperationMatches{'Blob.List'})
+ )
+ OR 
+ (
+  @Resource[Microsoft.Storage/storageAccounts/blobServices/containers:name] StringEquals 'blobs-example-container'
+ )
+)
+EOT
     }
   }
 }
